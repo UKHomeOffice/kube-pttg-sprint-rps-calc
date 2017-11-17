e4492004155144d19c0591a6cb2c37d49c05a838
Stuart Craigen <s.craigen@mac.com>
2017-11-17 10:46:00 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kube-pttg-sprint-rps-calc
---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export WHITELIST=${WHITELIST:-0.0.0.0/0}
+
+if [ $ENVIRONMENT == "prod" ]
+then
+    export KUBE_TOKEN=${PROD_KUBE_TOKEN}
+    export DNS_PREFIX=
+else
+    export DNS_PREFIX=${ENVIRONMENT}.notprod.
+fi
+
+cd kd
+kd --insecure-skip-tls-verify --timeout 5m0s \
+   --file ingress.yaml \
+   --file service.yaml \
+   --file deployment.yaml
